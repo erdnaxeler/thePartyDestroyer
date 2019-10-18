ea5bddf925425c414054189fc58b108df0224275
erdnaxeler <moulart.alexandre.ampgmail.com>
2019-10-19 00:49:19 +0200

    First test deploy

---
+++ Resources/JavaScript/main.js
@@ -0,0 +1,28 @@
+import questionsArray from 'questionsArray.js';
+
+console.log(questionsArray.length);
+
+//Wait until DOM content is loaded to register events
+window.addEventListener('DOMContentLoaded', registerEvents, false);
+function registerEvents (e) {
+		document.getElementById("activator").addEventListener('click', getItGoing, true);
+};
+
+
+function relPage (e) {
+};
+
+function getItGoing (e) {
+	let decidingNumber = Math.floor(Math.random()*questionsArray.length);
+	console.log(decidingNumber);
+	let finalQuestion = questionsArray[decidingNumber];
+		if (questionsArray.length !== 0) {
+					document.getElementById("questionHolder").innerHTML = finalQuestion
+					questionsArray.splice(decidingNumber,1);
+				} else {
+					document.getElementById("questionHolder").innerHTML =
+						"Sorry, there are no more new questions. Please refresh the page for a new game."
+			}
+		};
+
+
